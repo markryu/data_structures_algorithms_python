84e966660f299ad95f10051160d57139f382c343
Arvin Mark Atienza <arvinmarkatienza@Arvins-MacBook-Air.local>
2020-03-10 21:32:09 +0800

    added some learnings on strings and guess and check algorithm

---
+++ SimplePrograms/strings.py
@@ -0,0 +1,24 @@
+"""
+Strings are sequence of characters.
+
+Operators ---- == > < ----
+
+Indexing works on strings.
+    - This means that you can get an element from the string depending on index.
+
+Slicing also works on strings.
+    - A part of the string can be taken away into a new copy.
+string[start:stop:step]
+    - This is a sample of a slice
+        string[::-1] ---> reverse
+
+Strings are immutable
+    - that means strings are write-protected
+
+Strings can also be looped over
+    - it is similar to an array of characters.
+
+You have to redefine immutable objects in order to change them.
+    - That means that you have to create a new version of the item.
+
+"""
